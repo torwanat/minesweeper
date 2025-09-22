e6bc89eb5e975a2dbe051fa95c694dc3764b4f8e
torwanat <74968490+torwanat@users.noreply.github.com>
2025-09-22 14:53:54 +0300

    refactor game board, add foundations for statistics window

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import tkinter as tk
 global main_board
 global board_dimensions
 global game_window
-global status_label
+global main_menu_status
+global turn
 
 def validate_dimensions(height, width, mines):
     if not height.isdigit() or not width.isdigit() or not mines.isdigit():
@@ -20,43 +21,67 @@ def play_button_click(board_height, board_width, board_mines):
     if validate_dimensions(board_height, board_width, board_mines):
         start_game(int(board_height), int(board_width), int(board_mines))
     else:
-        update_status_label("Invalid dimensions!", "red")
+        global main_menu_status
+        main_menu_status.set("Invalid dimensions!")
         return
-
-
-def update_status_label(status_text, text_color):
-    global status_label
-    status_label.config(text=status_text, fg=text_color)
 
 
 def start_game(width, height, mines):
     global main_board
     global board_dimensions
     global game_window
+    global turn
+    global main_menu_status
 
-    update_status_label("Game ongoing...", "gray")
+    turn = tk.StringVar()
+    turn.set("Turn #1")
+
+    main_menu_status.set("Game ongoing...")
 
     game_window = tk.Toplevel(main_window)
     game_window.title("Game of Minesweeper")
 
+    board_frame = tk.Frame(game_window)
+
+    top_frame = tk.Frame(game_window)
+
+    result_label = tk.Label(top_frame)
+    title_label = tk.Label(top_frame, text="Minesweeper", font=("Bauhaus 93", 12))
+    turn_label = tk.Label(top_frame, textvariable=turn)
+
+    result_label.pack(side="left")
+    title_label.pack(side="left")
+    turn_label.pack(side="left")
+
     main_board = prepare_logical_board(width, height, mines)
-    tiles = prepare_tiles(width, height, game_window)
+    tiles = prepare_tiles(width, height, board_frame)
     board_dimensions = [width, height]
 
     for x in range(len(main_board)):
         for y in range(len(main_board[x])):
             main_board[x][y]["tile"] = tiles[x][y]
 
+    top_frame.pack(side="top")
+    board_frame.pack()
+
     game_window.mainloop()
+
+
+def increase_turn():
+    current_turn = turn.get()
+    next_turn = int(current_turn[-1]) + 1
+    turn.set(f'Turn #{next_turn}')
 
 
 def end_game(result):
     global game_window
+    global main_menu_status
+
     game_window.destroy()
     if result == "WIN":
-        update_status_label("Congratulations!", "green")
+        main_menu_status.set("Congratulations!")
     else:
-        update_status_label("Game over!", "red")
+        main_menu_status.set("Game over!")
 
 
 def prepare_logical_board(width, height, mines):
@@ -116,6 +141,7 @@ def left_click_on_tile(event):
     x = event.widget.grid_info()["row"]
     y = event.widget.grid_info()["column"]
 
+    increase_turn()
     uncover_tile(x, y)
 
 
@@ -177,20 +203,59 @@ def pick_color(value):
         return "#000000"
 
 
+def stats_button_click():
+    stats_data = []
+    show_stats(stats_data)
+
+
+def get_stats_data():
+    pass
+
+
+def show_stats(stats_data):
+    stats_window = tk.Toplevel(main_window)
+    stats_window.title("Statistics")
+
+    title_label = tk.Label(stats_window, text="Previous scores", font=("Arial", 15))
+    date_label = tk.Label(stats_window, text="Date")
+    duration_label = tk.Label(stats_window, text="Duration")
+    turns_label = tk.Label(stats_window, text="Turns")
+    result_label = tk.Label(stats_window, text="Result")
+    mines_left_label = tk.Label(stats_window, text="Mines left")
+
+    title_label.grid(row=0, column=2)
+    date_label.grid(row=1, column=0)
+    duration_label.grid(row=1, column=1)
+    turns_label.grid(row=1, column=2)
+    result_label.grid(row=1, column=3)
+    mines_left_label.grid(row=1, column=4)
+
+
+    stats_window.mainloop()
+
 def main():
-    global status_label
+    global main_menu_status
+    main_menu_status = tk.StringVar()
 
     title_label = tk.Label(main_window, text="Minesweeper", font=("Bauhaus 93", 30))
     height_label = tk.Label(main_window, text="Height")
     width_label = tk.Label(main_window, text="Width")
     mines_label = tk.Label(main_window, text="Mines")
-    status_label = tk.Label(main_window, text="")
+    status_label = tk.Label(main_window, textvariable=main_menu_status)
 
     height_entry = tk.Entry(main_window)
     width_entry = tk.Entry(main_window)
     mines_entry = tk.Entry(main_window)
 
-    play_button = tk.Button(main_window, text="Play", command=lambda: play_button_click(height_entry.get(), width_entry.get(), mines_entry.get()))
+    buttons_frame = tk.Frame(main_window)
+
+    play_button = tk.Button(buttons_frame, text="Play", command=lambda: play_button_click(height_entry.get(), width_entry.get(), mines_entry.get()))
+    stats_button = tk.Button(buttons_frame, text="Stats", command=lambda: stats_button_click())
+    gap_label = tk.Label(buttons_frame, width=10)
+
+    play_button.pack(side="left")
+    gap_label.pack(side="left")
+    stats_button.pack(side="left")
 
     title_label.grid(row=0, column=1, padx=20, pady=20)
     height_label.grid(row=1, column=1, padx=20)
@@ -200,7 +265,7 @@ def main():
     mines_label.grid(row=5, column=1, padx=20)
     mines_entry.grid(row=6, column=1, padx=20)
     status_label.grid(row=7, column=1, padx=20)
-    play_button.grid(row=8, column=1, padx=20, pady=20)
+    buttons_frame.grid(row=8, column=1, padx=20, pady=20)
 
     main_window.mainloop()
 
